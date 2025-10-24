8aa826f4dfcdee3711cd1bb258291bf02a9f3998
Anthony Metzger <tony.m.metzger@gmail.com>
2025-10-24 18:35:15 -0400

    Add files via upload
    Just modified some of the code to fit the required tests. Please keep this in mind when viewing the project.

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+//PLEASE NOTE COMMENTS ON RIGHT PORTION OF CODE!!! THIS CODE IS WRITTED TO PASS THE INDICATED TEST, HOWEVER
+//IT GOES BEYOND THE SCOPE OF THE REQUIRED TEST. PLEASE TAKE NOTE OF THIS WHEN VIEWING THE UI!!
+
+
+
+
+
 /*Created by: Anthony Metzger
 Date: 23 October 2025
 /*in this project, I've decided to use an object to hold the denominations of currency
@@ -9,18 +16,18 @@ loop would be more efficient, especially when paired with a for...of loop to ite
 
 //declaring an object to hold the denominations of currency
 let cashierGiveBack = new Object();
-cashierGiveBack.hundred = 100.00;
+/*cashierGiveBack.hundred = 100.00;                                                                       DELETE COMMENT INDICATOR FROM THIS LINE
 cashierGiveBack.fifty = 50.00;
 cashierGiveBack.twenty = 20.00;
 cashierGiveBack.ten = 10.00;
-cashierGiveBack.five = 5.00;
+cashierGiveBack.five = 5.00;                                                                              AND THIS LINE TO ENDABLE 100s TO PENNIES  */
 cashierGiveBack.one = 1.00;
 cashierGiveBack.quarters = 0.25;
 cashierGiveBack.dime = 0.10;
 cashierGiveBack.nickel = 0.05;
 cashierGiveBack.penny = 0.01;
 //declaring an array to hold the count of each denomination to be given back as change
-let cashArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; 1
+let cashArray = [/*0, 0, 0, 0, 0,*/ 0, 0, 0, 0, 0];                                                       //DELETE THE COMMENT IN THIS LINE TO ENABLE 100s TO PENNIES
 //function to check if customer paid enough, gets passed customer payment and price of item
 function didCustomerPayEnough(customer, price) {
   console.log("Calculating if customer paid enough...");
@@ -29,13 +36,13 @@ function didCustomerPayEnough(customer, price) {
   let p = price;
   //if statement to see if customer paid less than price, if so, return false
   if (c < p) {
-    document.getElementById("changeDue").innerText = "Insufficient Payment";
+    document.getElementById("amounnt-due").innerText = "Insufficient Payment";
     console.log("Customer did not pay enough.");
     return false;
   }
     //in case the user attempts to input negative values
   else if (c <= 0 || p <= 0) {
-    document.getElementById("changeDue").innerText = "Invalid Input";
+    document.getElementById("amounnt-due").innerText = "Invalid Input";
     return false;
   }
     //else if statement to see if customer paid enough or more, if so, return true
@@ -53,6 +60,7 @@ function didCustomerPayEnough(customer, price) {
 //and determines how many of each denomination to give back. It also gets passed the
 //customer payment and price of item.
 function calculateChange(customer, price) {
+  
   //similar to the previous function, assigning parameters to local variables
   //and parsing them to floats for calculation purposes. I had problems with
   //JavaScript being 0.01 off in some calculations due to floating point precision.
@@ -67,7 +75,7 @@ function calculateChange(customer, price) {
   //parsing float due to problem stated above
   parseFloat(due);
   //updating HTML to show change due
-  document.getElementById("changeDue").innerText = ("Change Due: $" + due.toFixed(2));
+  document.getElementById("amount-due").innerText = ("$" + due.toFixed(2) + " due");
   //checking if customer paid enough, if not, return 0 to end function
   if (didCustomerPayEnough(c, p) == false) {
     return 0;
@@ -112,16 +120,31 @@ function displayChange() {
   //i discovered you can play audio files stored in your project folder.
   let audio = new Audio('assets/chaching.mp3');
   audio.play();
-  document.getElementById("hundred").innerHTML = "Hundreds: " + cashArray[0];
-  document.getElementById("fifty").innerHTML = "Fifties: " + cashArray[1];
-  document.getElementById("twenty").innerHTML = "Twenties: " + cashArray[2]; 
-  document.getElementById("ten").innerHTML = "Tens: " + cashArray[3]; 
-  document.getElementById("five").innerHTML = "Fives: " + cashArray[4];
-  document.getElementById("one").innerHTML = "Ones: " + cashArray[5];
-  document.getElementById("quarters").innerHTML = "Quarters: " + cashArray[6];
-  document.getElementById("dimes").innerHTML = "Dimes: " + cashArray[7]; 
-  document.getElementById("nickels").innerHTML = "Nickels: " + cashArray[8];
-  document.getElementById("pennies").innerHTML = "Pennies: " + cashArray[9];
+  //ENABLE FOR ALT CODE vv
+  /*document.getElementById("hundreds-output").innerHTML = "Hundreds: " + cashArray[0]; 
+  document.getElementById("fifties-output").innerHTML = "Fifties: " + cashArray[1];
+  document.getElementById("twenties-output").innerHTML = "Twenties: " + cashArray[2]; 
+  document.getElementById("tens-output").innerHTML = "Tens: " + cashArray[3]; 
+  document.getElementById("fives-output").innerHTML = "Fives: " + cashArray[4];                              DELETE THE COMMENT START AND END TO ENABLE
+  document.getElementById("dollars-output").innerHTML = "Ones: " + cashArray[5];                             THE CODE BETWEEN THE INDICATORS TO
+  document.getElementById("quarters-output").innerHTML = "Quarters: " + cashArray[6];                        ENABLE MY VERSION OF THE CODE THAT
+  document.getElementById("dimes-output").innerHTML = "Dimes: " + cashArray[7];                              DISPLAYS 100s TO PENNIES!!!
+  document.getElementById("nickels-output").innerHTML = "Nickels: " + cashArray[8];
+  document.getElementById("pennies-output").innerHTML = "Pennies: " + cashArray[9]; */
+  //ENABLE FOR ALT CODE ^^
+
+
+
+  //DELETE FOR ALT CODE ^^
+  document.getElementById("dollars-output").innerHTML = cashArray[0];
+  document.getElementById("quarters-output").innerHTML = cashArray[1];
+  document.getElementById("dimes-output").innerHTML = cashArray[2];
+  document.getElementById("nickels-output").innerHTML = cashArray[3];
+  document.getElementById("pennies-output").innerHTML = cashArray[4];
+  //DELETE FOR ALT CODE ^^
+
+
+
   //logging cashierGiveBack key and value pairs to console for testing and debugging purposes
   for (let key in cashierGiveBack) {
     console.log(key + ": " + cashierGiveBack[key]);
